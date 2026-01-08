c4579b8bb9ba978bdbc8b575c082285a4f136d44
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2026-01-08 03:21:04 -0500

    Fix treesitter config for newer API

--- nvim/lua/ameya/plugins/treesitter.lua
+++ nvim/lua/ameya/plugins/treesitter.lua
@@ -1,35 +1,34 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "cpp",
-        "python",
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "markdown",
-        "markdown_inline",
-        "json",
-        "yaml",
-        "bash",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "c",
+      "cpp",
+      "python",
+      "html",
+      "css",
+      "javascript",
+      "typescript",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "markdown",
+      "markdown_inline",
+      "json",
+      "yaml",
+      "bash",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+    },
+  },
 }
